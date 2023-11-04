97b8abc6add81f94e73b6f1de3276256b36ce1d8
IrinaZinchenko <zinahohol@gmail.com>
2023-11-04 14:19:00 +0300

    apdate

--- learn-frontend/reg-ex/src/index.js
+++ learn-frontend/reg-ex/src/index.js
@@ -1,38 +1,6 @@
 import './index.scss';
 
-// Пример
-const example = document.querySelector(".example");
-
-const input = example.querySelector("#example-input");
-const btn = example.querySelector(".example-btn");
-const result = example.querySelector('.example-result');
-
-btn.addEventListener("click", () => {
-  const text = input.value;
-
-  if (text) {
-    const words = text.split(" ");
-    const revWords = [];
-
-    for (let word of words) {
-      let revWord = "";
-
-      for (let i = word.length - 1; i >= 0; --i) {
-        revWord += word[i];
-      }
-
-      revWords.push(revWord);
-    }
-
-    const revText = revWords.join(" ");
-
-    result.textContent = revText;
-
-    input.value = "";
-  }
-});
-
-// Задача 12.1
+// Задача 13.1
 const task1 = document.querySelector('.task-1');
 const input1 = task1.querySelector('#input-1');
 const button1 = task1.querySelector('.btn-1');
@@ -41,40 +9,25 @@ const result1 = task1.querySelector('.result-1');
 button1.addEventListener("click", () => {
   const phoneNumber = input1.value;
 
-  const array = phoneNumber.split('');
+  let result = 'Некорректно введен номер. Попробуйте снова';
 
-  let result = `<p>Ваш ввод: ${phoneNumber}</p><p>Ваш телефонный номер в нормализованном виде: Некорректно введен номер. Попробуйте снова</p>`;
+  let onlyNumbers = phoneNumber.match(/\d/g).join('');
 
-  let noSpecialSymbols = array.filter((item) => item !== ' ').filter((item) => item !== '(').filter((item) => item !== ')').filter((item) => item !== '-');
+  const numberLengthRegEx = /^.{11}$/;
 
-  if (noSpecialSymbols[0] === '+') {
-    noSpecialSymbols.shift();
-  }
-
-  if (noSpecialSymbols.every((item) => item === '0' || item === '1' || item === '2' || item === '3' || item === '4' || item === '5' || item === '6' || item === '7' || item === '8' || item === '9')) {
-    // переписать цифры в массив
-    if (noSpecialSymbols.length === 11) {
-      if (noSpecialSymbols[0] === '7') {
-        noSpecialSymbols.unshift('+');
-        // заменить шифт на + и слайс строки с 1 символа до конца
-        result = `<p>Ваш ввод: ${phoneNumber}</p><p>Ваш телефонный номер в нормализованном виде: ${noSpecialSymbols.join('')}</p>`;
-      } else if (noSpecialSymbols[0] === '8') {
-        noSpecialSymbols.shift();
-        noSpecialSymbols.unshift('7');
-        noSpecialSymbols.unshift('+');
-        // заменить шифт и аншифт на +7 и слайс строки с 1 символа до конца
-        result = `<p>Ваш ввод: ${phoneNumber}</p><p>Ваш телефонный номер в нормализованном виде: ${noSpecialSymbols.join('')}</p>`;
-      }
+  if (numberLengthRegEx.test(onlyNumbers)) {
+    if (onlyNumbers[0] === '7' || onlyNumbers[0] === '8') {
+      result = '+7' + onlyNumbers.slice(1);
     }
   }
 
-  result1.innerHTML = result;
+  result1.innerHTML = `<p>Ваш ввод: ${phoneNumber}</p><p>Ваш телефонный номер в нормализованном виде: ${result}</p>`;
 
   input1.value = '';
 });
 
 
-// Задача 12.2
+// Задача 13.2
 const task2 = document.querySelector('.task-2');
 const input2 = task2.querySelector('#input-2');
 const button2 = task2.querySelector('.btn-2');
@@ -83,36 +36,15 @@ const result2 = task2.querySelector('.result-2');
 button2.addEventListener("click", () => {
   const string = input2.value;
 
-  let resultArray = [];
-  let number = '';
-  const arrayOfNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let result = string.match(/(-?\d+(\.\d+)?)/g).join(', ');
 
-  for (let i = 0; i < string.length; i++) {
-    if (arrayOfNumbers.includes(string[i])) {
-      number += string[i];
-    } else {
-      if (number) {
-        resultArray.push(number);
-        number = '';
-      }
-      // внести условие с последним числом внутрь цикла при помощи позиции [string.length - 1]
-    }
-  }
-
-  // если последний символ - число
-  if (number) {
-    resultArray.push(number);
-  }
-
-  let result = `<p>Ваш ввод: ${string}</p><p>Числа внутри вашей строки: ${resultArray.join(', ')}</p>`;
-
-  result2.innerHTML = result;
+  result2.innerHTML = `<p>Ваш ввод: ${string}</p><p>Числа внутри вашей строки: ${result}</p>`;
 
   input2.value = '';
 });
 
 
-// Задача 12.3
+// Задача 13.3
 const task3 = document.querySelector('.task-3');
 const input3 = task3.querySelector('#input-3');
 const button3 = task3.querySelector('.btn-3');
@@ -121,30 +53,19 @@ const result3 = task3.querySelector('.result-3');
 button3.addEventListener("click", () => {
   const str = input3.value;
 
-  let result = '';
+  let capitl = str.match(/[A-Z]/g).length;
+  let lowercase = str.match(/[a-z]/g).length;
 
-  // код
-  // Заглавные 65-90 строчные 97-122
-  let capitl = 0;
-  let lowercase = 0;
+  // London is the Capital of Great Britan
+  // мал - 27, бол - 4
 
-  for (let i = 0; i < str.length; i++) {
-    if (str.codePointAt(i) >= 65 && str.codePointAt(i) <= 90) {
-      ++capitl;
-    } else if (str.codePointAt(i) >= 97 && str.codePointAt(i) <= 122) {
-      ++lowercase;
-    }
-  }
-
-  result = `<p>Ваш ввод: ${str}</p><p>Заглавных букв: ${capitl}</p><p>Строчных букв: ${lowercase}</p>`;
-
-  result3.innerHTML = result;
+  result3.innerHTML = `<p>Ваш ввод: ${str}</p><p>Заглавных букв: ${capitl}</p><p>Строчных букв: ${lowercase}</p>`;
 
   input3.value = '';
 });
 
 
-// Задача 12.4
+// Задача 13.4
 const task4 = document.querySelector('.task-4');
 const input4 = task4.querySelector('#input-4');
 const button4 = task4.querySelector('.btn-4');
@@ -153,10 +74,7 @@ const result4 = task4.querySelector('.result-4');
 button4.addEventListener("click", () => {
   const str = input4.value;
 
-  let result = '';
-
-  // код
-  const array = str.split(' ');
+  const array = str.match(/[А-Яа-яЁёa-zA-Z]+/g);
 
   let longestWord = array[0];
   for (let i = 1; i < array.length; i++) {
@@ -165,249 +83,201 @@ button4.addEventListener("click", () => {
     }
   }
 
-  result = `<p>Ваш ввод: ${str}</p><p>Самое длинное слово: ${longestWord}</p>`;
-
-  result4.innerHTML = result;
+  result4.innerHTML = `<p>Ваш ввод: ${str}</p><p>Самое длинное слово: ${longestWord}</p>`;
 
   input4.value = '';
 });
 
 
-// Задача 12.5
+// Задача 13.5
 const task5 = document.querySelector('.task-5');
 const input5 = task5.querySelector('#input-5');
 const button5 = task5.querySelector('.btn-5');
 const result5 = task5.querySelector('.result-5');
 
 button5.addEventListener("click", () => {
-  const str = input5.value;
+  const word = input5.value;
 
-  let result = '';
-
-  // код
   let reverseWord = '';
 
-  for (let i = (str.length - 1); i >= 0; i--) {
-    reverseWord += str[i];
-  }
-  // преобразовать строку в массив, применить метод массива чтобы сделать реверс массива и склеить обратно в строку
+  word.replace(/[а-яА-Я\w]/g, (letter) => reverseWord = letter + reverseWord);
 
-  let isChangeling;
-  if (reverseWord === str) {
-    isChangeling = true;
-  } else {
-    isChangeling = false;
-  }
+  let result = Boolean(word === reverseWord);
 
-  result = `<p>Ваш ввод: ${str}</p><p>Является ли слово перевертышем: ${isChangeling}</p>`;
-
-  result5.innerHTML = result;
+  result5.innerHTML = `<p>Ваш ввод: ${word}</p><p>Является ли слово перевертышем: ${result}</p>`;
 
   input5.value = '';
 });
 
 
-// Задача 12.6
-const task6 = document.querySelector('.task-6');
-const input6 = task6.querySelector('#input-6');
-const button6 = task6.querySelector('.btn-6');
-const result6 = task6.querySelector('.result-6');
+// // Задача 13.6
+// const task6 = document.querySelector('.task-6');
+// const input6 = task6.querySelector('#input-6');
+// const button6 = task6.querySelector('.btn-6');
+// const result6 = task6.querySelector('.result-6');
 
-button6.addEventListener("click", () => {
-  const str = input6.value;
+// button6.addEventListener("click", () => {
+//   const str = input6.value;
 
-  let result = '';
+//   let newStr = str.replace(/[ ]+\b\b[ ]+/g, '');
+//   // let newStr = str.match(/^([ ]+\b)^(\b[ ]+)/g);
+//   console.log(str);
+//   console.log(newStr);
 
-  // код
-  let array = str.split('');
-  // очистить через все фильтр от всех пробелов, а потом соединить обратно в строку через пробел
-  while (array[0] === ' ') {
-    array.shift();
-  }
+//   result6.innerHTML = `<p>Ваш ввод без лишних пробелов: ${newStr}</p>`;
 
-  while (array[array.length - 1] === ' ') {
-    array.pop();
-  }
-
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] === ' ' && array[i + 1] === ' ') {
-      array.splice(i, 1);
-      i--;
-    }
-  }
-
-  let resultStr = array.join('');
-
-  result = `<p>Ваш ввод без лишних пробелов: ${resultStr}</p>`;
-
-  result6.innerHTML = result;
-
-  input6.value = '';
-});
+//   input6.value = '';
+// });
 
 
-// Задача 12.7
-const task7 = document.querySelector('.task-7');
-const input7 = task7.querySelector('#input-7');
-const button7 = task7.querySelector('.btn-7');
-const result7 = task7.querySelector('.result-7');
+// // Задача 12.7
+// const task7 = document.querySelector('.task-7');
+// const input7 = task7.querySelector('#input-7');
+// const button7 = task7.querySelector('.btn-7');
+// const result7 = task7.querySelector('.result-7');
 
-button7.addEventListener("click", () => {
-  const str = input7.value;
+// button7.addEventListener("click", () => {
+//   const str = input7.value;
 
-  let result = '';
+//   let result = str.replace(/\b[a-z]/g, `/\b[A-Z]/g`);
 
-  // код
-  let array = str.split(' ');
-  // можно преобразовать цикл в метод map
-  let newArray = [];
-  for (let word of array) {
-    let newStr = word.slice(0, 1).toUpperCase();
-    newStr += word.slice(1);
-    newArray.push(newStr);
-  }
+//   result7.innerHTML = `<p>Ваш ввод: ${str}</p><p>Результат: ${result}</p>`;
 
-  let resultStr = newArray.join(' ');
-
-  result = `<p>Ваш ввод: ${str}</p><p>Результат: ${resultStr}</p>`;
-
-  result7.innerHTML = result;
-
-  input7.value = '';
-});
+//   input7.value = '';
+// });
 
 
-// Задача 12.8*
-const task8 = document.querySelector('.task-8');
-const input8 = task8.querySelector('#input-8');
-const button8 = task8.querySelector('.btn-8');
-const result8 = task8.querySelector('.result-8');
+// // Задача 12.8*
+// const task8 = document.querySelector('.task-8');
+// const input8 = task8.querySelector('#input-8');
+// const button8 = task8.querySelector('.btn-8');
+// const result8 = task8.querySelector('.result-8');
 
-button8.addEventListener("click", () => {
-  const str = input8.value;
+// button8.addEventListener("click", () => {
+//   const str = input8.value;
 
-  let result = '';
+//   let result = '';
 
-  // код
-  let equal = str.indexOf("=");
-  let plus = str.indexOf("+");
-  let minus = str.indexOf("-");
-  let multiplication = str.indexOf("*");
-  let division = str.indexOf("/");
+//   // код
+//   let equal = str.indexOf("=");
+//   let plus = str.indexOf("+");
+//   let minus = str.indexOf("-");
+//   let multiplication = str.indexOf("*");
+//   let division = str.indexOf("/");
 
-  let operation = '';
-  if (plus !== -1) {
-    operation = plus;
-  } else if (minus !== -1) {
-    operation = minus;
-  } else if (multiplication !== -1) {
-    operation = multiplication;
-  } else {
-    operation = division;
-  }
+//   let operation = '';
+//   if (plus !== -1) {
+//     operation = plus;
+//   } else if (minus !== -1) {
+//     operation = minus;
+//   } else if (multiplication !== -1) {
+//     operation = multiplication;
+//   } else {
+//     operation = division;
+//   }
 
-  let num1 = parseInt(str.slice(0, operation));
-  let num2 = parseInt(str.slice((operation + 1), equal));
-  let num3 = parseInt(str.slice(equal + 1));
+//   let num1 = parseInt(str.slice(0, operation));
+//   let num2 = parseInt(str.slice((operation + 1), equal));
+//   let num3 = parseInt(str.slice(equal + 1));
 
-  let bool = false;
+//   let bool = false;
 
-  switch (str[operation]) {
-    case '+':
-      if (num1 + num2 === num3) {
-        bool = true;
-      }
-      break;
+//   switch (str[operation]) {
+//     case '+':
+//       if (num1 + num2 === num3) {
+//         bool = true;
+//       }
+//       break;
 
-    case '-':
-      if (num1 - num2 === num3) {
-        bool = true;
-      }
-      break;
+//     case '-':
+//       if (num1 - num2 === num3) {
+//         bool = true;
+//       }
+//       break;
 
-    case '*':
-      if (num1 * num2 === num3) {
-        bool = true;
-      }
-      break;
+//     case '*':
+//       if (num1 * num2 === num3) {
+//         bool = true;
+//       }
+//       break;
 
-    case '/':
-      if (num1 / num2 === num3) {
-        bool = true;
-      }
-      break;
-  }
+//     case '/':
+//       if (num1 / num2 === num3) {
+//         bool = true;
+//       }
+//       break;
+//   }
 
-  result = `<p>Ваш ввод: ${str}</p><p>Результат: ${bool}</p>`;
+//   result = `<p>Ваш ввод: ${str}</p><p>Результат: ${bool}</p>`;
 
-  result8.innerHTML = result;
+//   result8.innerHTML = result;
 
-  input8.value = '';
-});
+//   input8.value = '';
+// });
 
 
-// Задача 12.9*
-const task9 = document.querySelector('.task-9');
-const input9 = task9.querySelector('#input-9');
-const button9 = task9.querySelector('.btn-9');
-const result9 = task9.querySelector('.result-9');
+// // Задача 12.9*
+// const task9 = document.querySelector('.task-9');
+// const input9 = task9.querySelector('#input-9');
+// const button9 = task9.querySelector('.btn-9');
+// const result9 = task9.querySelector('.result-9');
 
-button9.addEventListener("click", () => {
-  const str = input9.value;
+// button9.addEventListener("click", () => {
+//   const str = input9.value;
 
-  let result = '';
+//   let result = '';
 
-  // цикл для заполнения массива чисел
-  let arrayOfNumbers = [];
-  let number = '';
-  const arrayForIf1 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//   // цикл для заполнения массива чисел
+//   let arrayOfNumbers = [];
+//   let number = '';
+//   const arrayForIf1 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-  for (let i = 0; i < str.length; i++) {
-    if (arrayForIf1.includes(str[i])) {
-      number += str[i];
-    } else {
-      if (number) {
-        arrayOfNumbers.push(number);
-        number = '';
-      }
-    }
-  }
+//   for (let i = 0; i < str.length; i++) {
+//     if (arrayForIf1.includes(str[i])) {
+//       number += str[i];
+//     } else {
+//       if (number) {
+//         arrayOfNumbers.push(number);
+//         number = '';
+//       }
+//     }
+//   }
 
-  if (number) {
-    arrayOfNumbers.push(number);
-  }
+//   if (number) {
+//     arrayOfNumbers.push(number);
+//   }
 
-  // цикл для заполнения массива знаков операций
-  let arrayOfSigns = [];
-  const arrayForIf2 = ['+', '-', '*', '/'];
+//   // цикл для заполнения массива знаков операций
+//   let arrayOfSigns = [];
+//   const arrayForIf2 = ['+', '-', '*', '/'];
 
-  for (let i = 0; i < str.length; i++) {
-    if (arrayForIf2.includes(str[i])) {
-      arrayOfSigns.push(str[i]);
-    }
-  }
+//   for (let i = 0; i < str.length; i++) {
+//     if (arrayForIf2.includes(str[i])) {
+//       arrayOfSigns.push(str[i]);
+//     }
+//   }
 
-  // получение результата
-  let count = 0;
-  let numResult = arrayOfNumbers.reduce((result, item) => {
-    if (arrayOfSigns[count] === '+') {
-      count++;
-      return parseInt(result) + parseInt(item);
-    } else if (arrayOfSigns[count] === '-') {
-      count++;
-      return parseInt(result) - parseInt(item);
-    } else if (arrayOfSigns[count] === '*') {
-      count++;
-      return parseInt(result) * parseInt(item);
-    } else {
-      count++;
-      return parseInt(result) / parseInt(item);
-    }
-  });
+//   // получение результата
+//   let count = 0;
+//   let numResult = arrayOfNumbers.reduce((result, item) => {
+//     if (arrayOfSigns[count] === '+') {
+//       count++;
+//       return parseInt(result) + parseInt(item);
+//     } else if (arrayOfSigns[count] === '-') {
+//       count++;
+//       return parseInt(result) - parseInt(item);
+//     } else if (arrayOfSigns[count] === '*') {
+//       count++;
+//       return parseInt(result) * parseInt(item);
+//     } else {
+//       count++;
+//       return parseInt(result) / parseInt(item);
+//     }
+//   });
 
-  result = `<p>Ваш ввод: ${str}</p><p>Результат: ${numResult}</p>`;
+//   result = `<p>Ваш ввод: ${str}</p><p>Результат: ${numResult}</p>`;
 
-  result9.innerHTML = result;
+//   result9.innerHTML = result;
 
-  input9.value = '';
-});
+//   input9.value = '';
+// });
